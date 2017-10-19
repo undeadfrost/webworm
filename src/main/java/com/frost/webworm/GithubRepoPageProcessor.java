34119f5fd89efd12593f30d17be339ceb5421737
undeadfrost <undeadfrost@live.com>
2017-10-19 18:09:44 +0800

    测试2

--- src/main/java/com/frost/webworm/GithubRepoPageProcessor.java
+++ src/main/java/com/frost/webworm/GithubRepoPageProcessor.java
@@ -7,17 +7,25 @@ import us.codecraft.webmagic.processor.PageProcessor;
 
 public class GithubRepoPageProcessor implements PageProcessor {
 
-    private Site site = Site.me().setRetrySleepTime(3).setSleepTime(100);
+    public final String listUrl = "\\?site_id=CMSxy&cat_id=10011&cur_page=\\d+";
+    public final String cssUrl = "div.commonPage form";
+
+    private Site site = Site.me().setDomain("www.xianyang.gov.cn").setSleepTime(100).setUserAgent(
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31");
+
 
     @Override
     // process是定制爬虫逻辑的核心接口，在这里编写抽取逻辑
     public void process(Page page) {
-        // 过滤获取列表文章链接
-        page.addTargetRequests(page.getHtml().xpath("//div[@class=\"item_title\"]").links().regex("/xyxw/jryw/\\w+\\.htm").all());
-        //
-        page.putField("title", page.getHtml().xpath("//div[@id=\"info_title\"]/text()").toString());
-        page.putField("release_time", page.getHtml().xpath("//span[@id=\"info_released_dtime\"]/text()").toString());
-        page.putField("content", page.getHtml().xpath("//div[@id=\"info_content\"]/html()").toString());
+        // 列表页
+        if (page.getUrl().regex(listUrl).match()){
+            page.addTargetRequests(page.getHtml().xpath("//div[@class=\"item_title\"]").links().regex("/xyxw/jryw/\\w+\\.htm").all());
+            page.addTargetRequests(page.getHtml().css(cssUrl).links().regex(listUrl).all());
+        } else {
+            page.putField("title", page.getHtml().xpath("//div[@id=\"info_title\"]/text()"));
+            page.putField("release_time", page.getHtml().xpath("//span[@id=\"info_released_dtime\"]/text()"));
+            page.putField("content", page.getHtml().xpath("//div[@id=\"info_content\"]/html()"));
+        }
     }
 
 
@@ -27,6 +35,6 @@ public class GithubRepoPageProcessor implements PageProcessor {
     }
 
     public static void main(String[] args){
-        Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.xianyang.gov.cn/info/iList.jsp?cat_id=10011").thread(1).run();
+        Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.xianyang.gov.cn/info/iList.jsp?site_id=CMSxy&cat_id=10011&cur_page=1").thread(1).run();
     }
 }
