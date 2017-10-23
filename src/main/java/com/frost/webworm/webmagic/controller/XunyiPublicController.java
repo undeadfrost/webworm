ce48da940e70001e43e4bac5c867da95200eab94
undeadfrost <undeadfrost@live.com>
2017-10-23 17:54:13 +0800

    添加www.xys.gov.cn信箱抓取

--- src/main/java/com/frost/webworm/webmagic/controller/XunyiPublicController.java
+++ src/main/java/com/frost/webworm/webmagic/controller/XunyiPublicController.java
@@ -1,7 +1,7 @@
 package com.frost.webworm.webmagic.controller;
 
+import com.frost.webworm.webmagic.service.impl.MailRepoPageProcessor;
 import com.frost.webworm.webmagic.service.impl.NewsPipeline;
-import com.frost.webworm.webmagic.service.impl.XunyiRepoPageProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +16,7 @@ public class XunyiPublicController {
     private NewsPipeline newsPipeline;
     @RequestMapping("/start")
     public String start(){
-        Spider.create(new XunyiRepoPageProcessor()).addUrl("http://www.snxunyi.gov.cn/info/iList.jsp?tm_id=7&node_id=&site_id=CMSxy&catalog_id=72&gk_index=&title=&cur_page=1").addPipeline(newsPipeline).thread(1).run();
+        Spider.create(new MailRepoPageProcessor()).addUrl("http://www.xys.gov.cn/appeal/list.jsp?model_id=4&dept_id=20&cur_page=1").thread(1).run();
         return "success";
     }
 }
