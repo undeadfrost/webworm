51bcd43a7dfb02b45bdac3e43fe4d8f4bc49610e
undeadfrost <undeadfrost@live.com>
2017-10-24 18:12:34 +0800

    添加咸阳地情网抓取

--- src/main/java/com/frost/webworm/webmagic/controller/XunyiPublicController.java
+++ src/main/java/com/frost/webworm/webmagic/controller/XunyiPublicController.java
@@ -2,6 +2,7 @@ package com.frost.webworm.webmagic.controller;
 
 import com.frost.webworm.webmagic.service.impl.MailRepoPageProcessor;
 import com.frost.webworm.webmagic.service.impl.NewsPipeline;
+import com.frost.webworm.webmagic.service.impl.XydqwRepoPageProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class XunyiPublicController {
     private NewsPipeline newsPipeline;
     @RequestMapping("/start")
     public String start(){
-        Spider.create(new MailRepoPageProcessor()).addUrl("http://www.xys.gov.cn/appeal/list.jsp?model_id=4&dept_id=20&cur_page=1").thread(1).run();
+        Spider.create(new XydqwRepoPageProcessor()).addUrl("http://www.xydqw.com/xwdt/dtfzkx/index.htm").thread(1).run();
         return "success";
     }
 }
---
+++ src/main/java/com/frost/webworm/webmagic/service/impl/XydqwRepoPageProcessor.java
@@ -0,0 +1,53 @@
+package com.frost.webworm.webmagic.service.impl;
+
+import com.frost.webworm.webmagic.entity.PublicEntity;
+import org.springframework.stereotype.Service;
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * Created by LB on 2017/10/21.
+ */
+@Service
+public class XydqwRepoPageProcessor implements PageProcessor {
+
+    public final String listUrl = "/xwdt/dtfzkx/inde\\w+\\.htm";
+    public final String cssUrl = "div.pageing span";
+
+    private Site site = Site.me().setDomain("www.snxunyi.gov.cn").setSleepTime(100).setUserAgent(
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31");
+
+    @Override
+    // process是定制爬虫逻辑的核心接口，在这里编写抽取逻辑
+    public void process(Page page) {
+        // 列表页
+        if (page.getUrl().regex(listUrl).match()) {
+            page.addTargetRequests(page.getHtml().links().regex("/\\d+/\\w+\\.htm").all());
+            page.addTargetRequests(page.getHtml().css(cssUrl).links().regex(listUrl).all());
+        } else {
+            PublicEntity publicEntity = new PublicEntity();
+            publicEntity.setPublicUrl(page.getUrl().toString());
+            publicEntity.setPublicIndex(page.getHtml().css("table.gk-table tr:nth-child(1) td:nth-child(2)").xpath("//td/text()").toString());
+            publicEntity.setInputDept(page.getHtml().css("table.gk-table tr:nth-child(2) td:nth-child(2)").xpath("//td/text()").toString());
+            publicEntity.setPublicTitle(page.getHtml().xpath("//div[@class=\"art-title\"]/text()").toString());
+            publicEntity.setReleaseTime(page.getHtml().css("table.gk-table tr:nth-child(2) td:nth-child(4)").xpath("//td/text()").toString());
+            publicEntity.setDocNo(page.getHtml().css("table.gk-table tr:nth-child(3) td:nth-child(2)").xpath("//td/text()").toString());
+            publicEntity.setWebsite("旬邑县政府");
+            publicEntity.setPublicColumn("法律法规");
+            if (publicEntity.getPublicTitle() == null) {
+                //skip this page
+                page.setSkip(true);
+            } else {
+                page.putField("repo", publicEntity);
+            }
+            // page.putField("content", page.getHtml().xpath("//div[@class=\"art-con\"]/html()"));
+        }
+    }
+
+    @Override
+    public Site getSite() {
+        return site;
+    }
+}
+
